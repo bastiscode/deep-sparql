ae1bd23b428ad7a3f5384d740fa9ed5af2fff4c6
Sebastian Walter <sebastian.walter98@gmail.com>
2023-06-27 15:26:16 +0200

    update evaluation script

--- scripts/evaluate_simple_questions.py
+++ scripts/evaluate_simple_questions.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from typing import Set, Optional, Tuple
 from multiprocessing import Pool
 
@@ -14,8 +15,11 @@ from deep_sparql.utils import (
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str, required=True)
     parser.add_argument("--target", type=str, required=True)
-    parser.add_argument("--predictions", type=str, required=True)
+    parser.add_argument("--prediction", type=str, required=True)
+    parser.add_argument("--save-invalid", type=str, default=None)
+    parser.add_argument("--save-incorrect", type=str, default=None)
     parser.add_argument("-n", "--num-processes", type=int, default=4)
     return parser.parse_args()
 
@@ -57,29 +61,43 @@ def calc_f1(pred_and_target: Tuple[str, str]) -> Optional[float]:
 
 
 def evaluate(args: argparse.Namespace):
+    inputs = load_text_file(args.input)
     targets = load_text_file(args.target)
-    predictions = load_text_file(args.predictions)
-    assert len(targets) == len(predictions), \
-        "expected the same number of targets and predictions"
+    predictions = load_text_file(args.prediction)
+    assert len(inputs) == len(targets) == len(predictions), \
+        "expected the same number of inputs, targets and predictions"
+
+    if args.save_invalid and os.path.exists(args.save_invalid):
+        os.remove(args.save_invalid)
+
+    if args.save_incorrect and os.path.exists(args.save_incorrect):
+        os.remove(args.save_incorrect)
 
     f1s = []
     invalid = 0
     with Pool(args.num_processes) as pool:
-        for f1 in tqdm(
-            pool.imap_unordered(
+        for i, f1 in tqdm(
+            enumerate(pool.imap(
                 calc_f1,
                 zip(predictions, targets),
                 chunksize=16
-            ),
+            )),
             desc="evaluating simple questions",
             total=len(targets),
             leave=False
         ):
+            if args.save_invalid:
+                with open(args.save_invalid, "a", encoding="utf8") as f:
+                    f.write(f"{inputs[i]}\t{predictions[i]}\n")
+            if args.save_incorrect and f1 is not None and f1 < 1.0:
+                with open(args.save_incorrect, "a", encoding="utf8") as f:
+                    f.write(f"{inputs[i]}\t{predictions[i]}\n")
+
             if f1 is None:
                 invalid += 1
                 f1s.append(0.0)
-                continue
-            f1s.append(f1)
+            else:
+                f1s.append(f1)
     print(
         f"Query-averaged F1: {100 * sum(f1s) / len(f1s):.2f} "
         f"({invalid:,} invalid, {100 * invalid / len(f1s):.2f}%)"
