245903a3d8b3ebd78595a6482dd39a9196ba63cb
Sebastian Walter <sebastian.walter98@gmail.com>
2023-08-11 13:43:11 +0200

    remove prints

--- src/deep_sparql/api/generator.py
+++ src/deep_sparql/api/generator.py
@@ -507,9 +507,6 @@ class SPARQLGenerator(corrector.TextCorrector):
                 kwargs["memory_padding_mask"],
                 kwargs.get("kv_cache", None)
             )
-            print(len(cache), [len(c) for c in cache])
-            print([[c.shape for c in ch] for ch in cache])
-            print(dec.shape)
             return dec, {"kv_cache": cache}
 
         def _kwargs_select_fn(
