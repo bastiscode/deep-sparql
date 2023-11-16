153eabc914af0a903c3a51d970b91fc8b5d54a09
Sebastian Walter <sebastian.walter98@gmail.com>
2023-11-16 18:41:32 +0100

    temp update to wikidata query logs script

--- scripts/prepare_wikidata_query_logs.py
+++ scripts/prepare_wikidata_query_logs.py
@@ -16,9 +16,6 @@ from prepare_data import prepare_sparqls
 
 def prepare_file(
     file: str,
-    entity_index: dict[str, list[str]],
-    entity_redir: dict[str, str],
-    property_index: dict[str, list[str]],
     args: argparse.Namespace
 ) -> tuple[int, int]:
     num_total = 0
@@ -84,6 +81,31 @@ def prepare_file(
             seen.add(sparql)
 
             sparql = " ".join(prefixes) + " " + sparql
+            with open(
+                os.path.join(args.output_dir, f"{source}.txt"),
+                "a",
+                encoding="utf8"
+            ) as of:
+                of.write(sparql + "\n")
+
+    return num_total, num_invalid
+
+
+def convert_file(
+    file: str,
+    output_file: str,
+    entity_index: dict[str, list[str]],
+    entity_redir: dict[str, str],
+    property_index: dict[str, list[str]],
+    args: argparse.Namespace
+):
+    num_total = 0
+    num_invalid = 0
+    with open(file, "r", encoding="utf8") as inf, \
+            open(output_file, "w", encoding="utf8") as of:
+        for line in inf:
+            num_total += 1
+            sparql = line.strip()
             sparqls = prepare_sparqls(
                 "wikidata",
                 sparql,
@@ -103,35 +125,23 @@ def prepare_file(
                 num_invalid += 1
                 continue
 
-            with open(
-                os.path.join(args.output_dir, f"{source}.txt"),
-                "a",
-                encoding="utf8"
-            ) as of:
+            for sparql in sparqls:
                 of.write(sparql + "\n")
 
     return num_total, num_invalid
 
 
 def prepare(args: argparse.Namespace):
-    entity_index, entity_redir = load_kg_index(
-        args.entity_index,
-        args.entity_redirects,
-        args.progress
-    )
-    property_index, _ = load_kg_index(
-        args.property_index,
-        progress=args.progress
-    )
-
     num_total = 0
     num_invalid = 0
-    for file in args.files:
+    for file in tqdm(
+        args.files,
+        desc="processing files",
+        leave=False,
+        disable=not args.progress
+    ):
         total, invalid = prepare_file(
             file,
-            entity_index,
-            entity_redir,
-            property_index,
             args
         )
         num_total += total
@@ -140,6 +150,17 @@ def prepare(args: argparse.Namespace):
     print(
         f"{num_invalid:,} / {num_total:,} invalid "
         f"({num_invalid / num_total:.1%}, organic_only={args.organic_only})"
+    )
+    return
+
+    entity_index, entity_redir = load_kg_index(
+        args.entity_index,
+        args.entity_redirects,
+        args.progress
+    )
+    property_index, _ = load_kg_index(
+        args.property_index,
+        progress=args.progress
     )
 
 
